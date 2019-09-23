a16bd435676bcb07a1a5dfd5d7855f0880ba5410
Angelica Peralta <aperalta015@gmail.com>
2019-09-23 12:29:33 -0400

    Assignment #3

--- ifGallery/homework.js
+++ ifGallery/homework.js
@@ -1,13 +1,11 @@
-
-
-
 // Sloth image "IMG_3883" by JohnHedtke is licensed under CC BY-NC-SA 2.0 https://search.creativecommons.org/photos/217faedd-d06a-48ba-91f7-f4d4c6dafa04
 // Puppy image "puppies" by www.petian.net is licensed under CC BY-NC-ND 2.0  https://search.creativecommons.org/photos/4f271583-e45d-4faf-bfc8-7291c7c1e3c3
 // dinosaur "Rupenhorn Dinosaur #4" by Kristian_Laban is licensed under CC BY-NC 2.0 https://search.creativecommons.org/photos/96213ba5-e9b6-4f2f-91e4-da28b9fcad22
+// horse "Horses in the Smokies" by coffey67 is licensed under CC BY-NC-ND 2.0 https://search.creativecommons.org/photos/0e25425a-a41c-49b0-9125-f0fa24c29fb4
+// cat "Cats @ $HOME" by Amayita is licensed under CC BY-NC-ND 2.0 https://search.creativecommons.org/photos/1ef8e2e5-6171-419f-b99e-f3c6c341720c
 
-
-var puppy, dinosaur, sloth;
-var pButton, dButton, sButton;
+var puppy, dinosaur, sloth, cat, horse;
+var pButton, dButton, sButton, cButton, hButton;
 
 var currentImage = 0;
 
@@ -17,6 +15,8 @@ function preload(){
 puppy = loadImage("images/puppy.jpg");
 dinosaur = loadImage("images/dinosaur.jpg");
 sloth = loadImage("images/sloth.jpg");
+cat = loadImage("images/cats.jpg");
+horse = loadImage("images/horse.jpg");
 }
 
 function setup() {
@@ -40,6 +40,16 @@ sButton.mousePressed(function(){
   currentImage = 2;
 });
 
+cButton = createButton("Cat");
+cButton.mousePressed(function(){
+  currentImage = 3;
+});
+
+hButton = createButton("Horse");
+hButton.mousePressed(function(){
+  currentImage = 4;
+});
+
 }
 
 function draw() {
@@ -47,15 +57,31 @@ function draw() {
   background(255);
 console.log("currentImage" + currentImage);
 if(currentImage== 0){
-  //show Puppy
+  //show puppy
   image(puppy,0,0,puppy.width/4, puppy.height/4);
 }else if(currentImage== 1){
+
 //show dinosaur
-image(dinosaur,0,0,dinosaur.width/4, dinosaur.height/4);
+  image(dinosaur,0,0,dinosaur.width/4, dinosaur.height/4);
 }else if(currentImage== 2){
+
   //show sloth
   image(sloth,0,0,sloth.width/4, sloth.height/4);
-}else if (currentImage==3) {
+}else if(currentImage== 3){
+
+  //show cat
+  image(cat,0,0,cat.width/4, cat.height/4);
+}else if(currentImage==4){
+
+  //show horse
+  image(horse,0,0,sloth.width/4, cat.height/4);
+}else if (currentImage == 5){
 
 }
+
+
+
+
+
+
 }
